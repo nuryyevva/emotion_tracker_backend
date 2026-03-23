516dff9d41d22645520a940ddcd6f1a2dd696fb5
KsenyaM2005 <118298442+KsenyaM2005@users.noreply.github.com>
2026-03-23 20:44:35 +0300

    use_cases: start (not tested)

--- src/use_cases/__init__.py
+++ src/use_cases/__init__.py
@@ -1,1 +1,36 @@
+from .register_user_use_case import RegisterUserUseCase
+from .authenticate_user_use_case import AuthenticateUserUseCase
+from .update_user_settings_use_case import UpdateUserSettingsUseCase
+from .submit_daily_checklist_use_case import SubmitDailyChecklistUseCase
+from .get_emotion_history_use_case import GetEmotionHistoryUseCase
+from .get_recommendation_use_case import GetRecommendationUseCase
+from .send_notification_use_case import SendNotificationUseCase
+from .analyze_trends_use_case import AnalyzeTrendsUseCase, TrendAnalysisResult
+from .export_data_use_case import ExportDataUseCase, ExportFormat
+from .process_payment_use_case import ProcessPaymentUseCase, SubscriptionPlan, PaymentResult
+from .analyze_notes_with_llm_use_case import AnalyzeNotesWithLLMUseCase, LLMSummary
+from .generate_report_use_case import GenerateReportUseCase, Period, ChartType, Metric, Report
 
+__all__ = [
+    'RegisterUserUseCase',
+    'AuthenticateUserUseCase',
+    'UpdateUserSettingsUseCase',
+    'SubmitDailyChecklistUseCase',
+    'GetEmotionHistoryUseCase',
+    'GetRecommendationUseCase',
+    'SendNotificationUseCase',
+    'AnalyzeTrendsUseCase',
+    'TrendAnalysisResult',
+    'ExportDataUseCase',
+    'ExportFormat',
+    'ProcessPaymentUseCase',
+    'SubscriptionPlan',
+    'PaymentResult',
+    'AnalyzeNotesWithLLMUseCase',
+    'LLMSummary',
+    'GenerateReportUseCase',
+    'Period',
+    'ChartType',
+    'Metric',
+    'Report'
+]
---
+++ src/use_cases/get_recommendation_use_case.py
@@ -0,0 +1,8 @@
+from uuid import UUID
+from typing import Optional, List
+
+from ..entities import Recommendation
+
+
+class GetRecommendationUseCase:
+    pass
